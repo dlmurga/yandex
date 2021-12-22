29a270b7e43fedd5331c240006d9cf71aad77b13
dlmurga <dl.murga@yandex.ru>
2021-12-22 16:40:20 +0300

    initial commit

---
+++ yandex.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+      version = "0.68.0"
+    }
+  }
+}
+
+provider "yandex" {
+  service_account_key_file = "/root/keys/key.json"
+  cloud_id = "b1grueplacatchduatm0"
+  folder_id = "b1ga9l1gdb6m45ek99hq"
+  zone = "ru-central1-a"
+}
+
+
+resource "yandex_compute_instance" "template" {
+  name = "vm1"
+
+
+  boot_disk {
+    initialize_params {
+      size = "15G"
+      image_id = "${var.image_id}"
+    }
+  }
+  network_interface {
+    subnet_id = "${var.subnet_id}"
+  }
+  resources {
+    cores = 2
+    memory = 2
+  }
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
